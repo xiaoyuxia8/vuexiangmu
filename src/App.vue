027417fb35789d1cf8d3837859ddd8c5b3d591f8
xiaoyuxia8 <1037493867@qq.com>
2019-07-10 18:13:24 +0800

    修改了底部导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,64 @@
 <template>
-<div>123
-  <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
+  <div class="contant">
+    123
+    <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
 
-  <!-- 底部 Tabbar 区域 -->
+    <!-- 底部 Tabbar 区域 -->
+    <!-- <nav data-v-7ba5bd90 class="mui-bar mui-bar-tab">
+      <a data-v-7ba5bd90 href="#/home" class="mui-tab-item-llb router-link-exact-active mui-active">
+        <span data-v-7ba5bd90 class="mui-icon mui-icon-home"></span>
+        <span data-v-7ba5bd90 class="mui-tab-label">首页</span>
+      </a>
+      <a data-v-7ba5bd90 href="#/member" class="mui-tab-item-llb">
+        <span data-v-7ba5bd90 class="mui-icon mui-icon-contact"></span>
+        <span data-v-7ba5bd90 class="mui-tab-label">会员</span>
+      </a>
+      <a data-v-7ba5bd90 href="#/shopcar" class="mui-tab-item-llb">
+        <span data-v-7ba5bd90 class="mui-icon mui-icon-extra mui-icon-extra-cart">
+          <span data-v-7ba5bd90 id="badge" class="mui-badge">0</span>
+        </span>
+        <span data-v-7ba5bd90 class="mui-tab-label">购物车</span>
+      </a>
+      <a data-v-7ba5bd90 href="#/search" class="mui-tab-item-llb">
+        <span data-v-7ba5bd90 class="mui-icon mui-icon-search"></span>
+        <span data-v-7ba5bd90 class="mui-tab-label">搜索</span>
+      </a>
+    </nav> -->
     <nav class="mui-bar mui-bar-tab">
-			<router-link class="mui-tab-item-llb" to="/home">
+			<a class="mui-tab-item mui-active" href="#tabbar">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</router-link>
-			<router-link class="mui-tab-item-llb" to="/member">
+			</a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
 				<span class="mui-icon mui-icon-contact"></span>
 				<span class="mui-tab-label">会员</span>
-			</router-link>
-			<router-link class="mui-tab-item-llb" to="/shopcar">
-				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
-					<span class="mui-badge" id="badge">0</span>
-				</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
 				<span class="mui-tab-label">购物车</span>
-			</router-link>
-			<router-link class="mui-tab-item-llb" to="/search">
+			</a>
+			
+			<a class="mui-tab-item" href="#tabbar-with-map">
 				<span class="mui-icon mui-icon-search"></span>
 				<span class="mui-tab-label">搜索</span>
-			</router-link>
+			</a>
 		</nav>
-</div>
+  </div>
 </template>
 
 <script>
-export default {
-  
-};
+export default {};
 </script>
 
 
 <style lang="scss" scoped>
-
+body {
+  background-color: pink;
+  .contant {
+    padding-top: 40px;
+    padding-top: 40px;
+    padding-bottom: 50px;
+    overflow-x: hidden;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,8 +6,12 @@ Vue.component(Header.name, Header);
 
 
 import MintUI from 'mint-ui'
+
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
 Vue.use(MintUI)
-import 'mint-ui/lib/style.css'
+
 
 // 1.1 路由的包
 // import VueRouter from 'vue-router'
