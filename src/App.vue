ebb8cd6588b5b7c14c7a5b258d744a36b55f6134
xiaoyuxia8 <1037493867@qq.com>
2019-07-10 16:26:36 +0800

    第一次添加

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div>123
+  <mt-header fixed title="黑马程序员·Vue项目"></mt-header>
+
+  <!-- 底部 Tabbar 区域 -->
+    <nav class="mui-bar mui-bar-tab">
+			<router-link class="mui-tab-item-llb" to="/home">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</router-link>
+			<router-link class="mui-tab-item-llb" to="/member">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item-llb" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+					<span class="mui-badge" id="badge">0</span>
+				</span>
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item-llb" to="/search">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</router-link>
+		</nav>
+</div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
